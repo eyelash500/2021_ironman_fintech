1ef28fb0fc89a15c6f1ae916f55117a67801694f
eyelash500 <eyelash.94500@gmail.com>
2021-09-30 23:45:44 +0800

    Finished day30

--- run_get_data.py
+++ run_get_data.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from legal_person import legal_daily_future_option
 from market_info import daily_market_info
 from stock_transaction import stock_transaction
+from fin_signal import fin_signal
 
 
 class daily_transaction:
@@ -28,7 +29,31 @@ class daily_transaction:
         print(f"=Finish: {datetime.now().strftime('%H%M%S')}")
 
 
+class signal:
+    """Show the signal"""
+
+    def __init__(self) -> None:
+        self.fin_signal = fin_signal()
+        self.fin_signal._get_db_data()  # get the row data
+
+    def show_signal(self) -> None:
+        """Create and show all the signal"""
+        signal_list = self.fin_signal.get_signal()
+        self._show_singnal(signal_list)
+
+    def _show_singnal(self, signal_list) -> None:
+        """Do showing the signal function"""
+        print(f"時間：{datetime.now().strftime('%Y-%M-%D %H:%M:%S')}")
+        print(f"＝＝訊號燈＝＝")
+        for title, signal in signal_list:
+            print(f"* {title}: {signal}")
+
+
 """實作測試"""
-# 匯入資料
-worker = daily_transaction()
-worker.save_to_db()
+# # 匯入資料
+# worker = daily_transaction()
+# worker.save_to_db()
+
+# 訊號燈印出
+signal = signal()
+signal.show_signal()
